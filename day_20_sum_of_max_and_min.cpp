b4b288d5520fd17503052f3ee0e3b7c2f31c30cc
AKrisadi <106695351+AKrisadi@users.noreply.github.com>
2023-09-01 23:48:51 +0530

    Create day_20_sum_of_max_and_min.cpp

---
+++ day_20_sum_of_max_and_min.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+int sumOfMaxMin(int arr[], int n){
+	// Write your code here.
+	 // Write your code here.
+
+    sort(arr, arr+n);
+
+    int x=arr[0];
+
+    int y=arr[n-1];
+
+    int z=x+y;
+
+    return z;
+}
